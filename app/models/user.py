69829f765e998351616caa7f074291022c447737
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2025-05-31 09:31:30 -0500

    Create user.py

---
+++ app/models/user.py
@@ -0,0 +1,9 @@
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    correo = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    ventas = db.relationship('Venta', back_populates='usuario')
