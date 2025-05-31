31a21d673416ef1a7d8f6d1de76bbcc0ecf9a6e5
Alejosor <alejandrogabrielsorianopalo@gmail.com>
2025-05-30 21:20:46 -0500

    Se agrega funcionamiento básico de resgistro y login de usuarios

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = 'clave-secreta'
+
     from .routes import main
+    from .auth import auth
+
     app.register_blueprint(main)
+    app.register_blueprint(auth)
+
     return app
