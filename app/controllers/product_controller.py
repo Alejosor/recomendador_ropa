d30ae6677eb59a85faf39f88ea13db4f78ca2068
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2025-05-31 10:43:16 -0500

    Update product_controller.py

--- app/controllers/product_controller.py
+++ app/controllers/product_controller.py
@@ -1,7 +1,13 @@
 from app.models.product import Product
-
+from app import db
 def obtener_top_productos(limit=10):
     return Product.query.order_by(Product.cant_ventas.desc()).limit(limit).all()
 
 def obtener_producto_por_id(producto_id):
     return Product.query.get(producto_id)
+
+def crear_producto(nombre, precio, categoria):
+    producto = Product(nombre=nombre, precio=precio, categoria=categoria)
+    db.session.add(producto)
+    db.session.commit()
+    return producto
